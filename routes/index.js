5e463c414249c286c69b73f3bed25d879a5bbbcc
Joseph Dailey <josephldailey@gmail.com>
2016-03-23 13:58:46 -0500

    systemd

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next) {
 		"GRADUATION, FATHER, STATUS, STATE FROM alumni ORDER BY INITIATED DESC LIMIT 25;",
 		function(err, top10){
 			if(err) console.log(err, top10);
-			console.log(top10);
 			res.render('index', { title: 'KHK Alumni Search', data: top10, last:top10[top10.length-1].NUMBER, error:err });		
 		}
 	);
@@ -18,7 +17,6 @@ router.get('/next', function(req, res, next) {
 		"GRADUATION, FATHER, STATUS, STATE FROM alumni WHERE NUMBER < "+req.query.lastn+" ORDER BY INITIATED DESC LIMIT 25;",
 		function(err, next10){
 			if(err) console.log(err, next10);
-			console.log(next10);
 			res.render('partials/results', { data: next10, last:next10[next10.length-1].NUMBER });		
 		}
 	);
