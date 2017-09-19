fea2d911e02f15ddd4e254af76cc63ed16e6756b
Joseph Dailey <josephldialey@gmail.com>
2017-09-19 09:10:11 +0000

    update build script

--- .application.sh
+++ .application.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sqlite3 ../khk-ssa/khk-access/db.sqlite "INSERT INTO apps (name, privilegeRequired, subdomain, icon) values (\"Roster\", 1, \"roster\", \"fa-group\");"
 sqlite3 ../khk-ssa/khk-access/db.sqlite "INSERT INTO apps (name, privilegeRequired, subdomain, icon) values (\"Roster Requests\", 2, \"roster-admin\", \"fa-user-plus\");"
-sqlite3 ../khk-ssa/khk-access/db.sqlite "INSERT INTO apps (name, privilegeRequired, subdomain, icon) values (\"Roster Backups\", 3, \"roster-backup\", \"fa-database\");"
+sqlite3 ../khk-ssa/khk-access/db.sqlite "INSERT INTO apps (name, privilegeRequired, subdomain, icon) values (\"Roster Backups\", 2, \"roster-backup\", \"fa-database\");"
